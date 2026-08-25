0fc205515bd0522d5fdb2f522b57762bd39c6487
Vincent GUILLOTTE <80900142+parime@users.noreply.github.com>
2026-08-25 06:15:58 +0200

    fix: corrige la categorie du fil d'ariane (admin -> tools) sur 11 ecrans (#12)

--- front/audit.php
+++ front/audit.php
@@ -25,7 +25,7 @@ Session::checkRight(PluginGrcmanagerAudit::$rightname, READ);
 Html::header(
     PluginGrcmanagerAudit::getTypeName(2),
     $_SERVER['PHP_SELF'],
-    'admin',
+    'tools',
     PluginGrcmanagerAudit::class
 );
 
